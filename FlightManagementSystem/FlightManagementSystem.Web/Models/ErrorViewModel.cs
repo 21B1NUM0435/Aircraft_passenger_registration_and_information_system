d0ab6cfe5ce68cd85fb20935e460a48ca2f5c4a4
shinebayar021 <144643908+shinebayar021@users.noreply.github.com>
2025-05-17 22:25:38 +0800

    qwert

--- FlightManagementSystem/FlightManagementSystem.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlightManagementSystem.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
